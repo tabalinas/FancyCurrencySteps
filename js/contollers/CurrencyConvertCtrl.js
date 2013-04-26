d7c9ee6e0fb351b5f092c98707bcb3da898fa5b8
tabalinas <tabalinas@gmail.com>
2013-04-27 02:25:29 +0400

    step 1: currency list

---
+++ js/contollers/CurrencyConvertCtrl.js
@@ -0,0 +1,11 @@
+﻿(function(app) {
+
+    app.ctrls.CurrencyConvertCtrl = function($scope) {
+        $scope.currencies = [];
+
+        angular.forEach(app.db.Currencies, function(currency) {
+            $scope.currencies.push(new app.models.Currency(currency));
+        });
+    };
+
+})(app);
---
+++ js/models/Currency.js
@@ -0,0 +1,12 @@
+﻿(function(app) {
+    
+    app.models.Currency = function(attrs) {
+        angular.extend(this, {
+            name: "",
+            ticker: "",
+            rate: 0,
+            value: 0,
+        }, attrs);
+    };
+
+})(app);
