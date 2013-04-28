0babb391e5a470bc8d8dc52fdf8c84cbd555fdcd
tabalinas <tabalinas@gmail.com>
2013-04-29 00:50:12 +0400

    step 3: conversion chart

--- js/contollers/CurrencyConvertCtrl.js
+++ js/contollers/CurrencyConvertCtrl.js
@@ -3,7 +3,8 @@
     var CONVERT_SERVICE_URL = "https://rate-exchange.appspot.com/currency";
 
     app.ctrls.CurrencyConvertCtrl = function($scope) {
-        var currencies = [];
+        var chart,
+            currencies = [];
 
         angular.forEach(app.db.Currencies, function(currency) {
             currencies.push(new app.models.Currency(currency));
@@ -16,7 +17,8 @@
             convert: function() {
                 var self = this,
                     valueToConvert = self.value,
-                    currencyTicker = self.currency;
+                    currencyTicker = self.currency,
+                    promises = [];
 
                 angular.forEach(self.currencies, function(currency) {
                     if(currency.ticker === currencyTicker) {
@@ -25,7 +27,7 @@
                         return;
                     }
 
-                    $.ajax({
+                    var promise = $.ajax({
                         type: "GET",
                         url: CONVERT_SERVICE_URL,
                         dataType: "jsonp",
@@ -37,9 +39,72 @@
                     }).done(function(data) {
                         currency.rate = data.rate;
                         currency.value = data.v;
-                        self.$apply();
                     });
+
+                    promises.push(promise);
                 });
+
+                $.when.apply(null, promises).done(function() {
+                    self.setChartData();
+                    self.$apply();
+                });
+            },
+
+            setChartData: function() {
+                var currencyTickers = [],
+                    values = [];
+
+                angular.forEach(this.currencies, function(currency) {
+                    currencyTickers.push(currency.ticker);
+                    values.push(currency.value);
+                });
+
+                chart.xAxis[0].setCategories(currencyTickers);
+                chart.series[0].setData(values);
+            }
+        });
+
+        chart = new Highcharts.Chart({
+            chart: {
+                renderTo: "chart",
+                type: "bar"
+            },
+            series: [{
+                name: "Value",
+                data: []
+            }],
+            title: {
+                text: "Value in world currencies"
+            },
+            xAxis: {
+                title: {
+                    text: null
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: null
+                },
+                labels: {
+                    overflow: "justify"
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true,
+                        formatter: function() {
+                            return this.y.toFixed(2);
+                        }
+                    }
+                }
+            },
+            credits: {
+                enabled: false
             }
         });
     };
